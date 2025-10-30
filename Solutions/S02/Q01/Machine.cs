e8c220e1f2c56469d94c63f949bf534f7b671df2
Trevor Barker <tmbarker9@gmail.com>
2025-10-29 21:59:57 -0700

    Solve S02 Q01

---
+++ Solutions/S02/Q01/Machine.cs
@@ -0,0 +1,69 @@
+﻿using Utilities.Geometry.Euclidean;
+
+namespace Solutions.S02.Q01;
+
+public class Machine
+{
+    private const char Nail = '*';
+    private const char Void = '.';
+    private const char Left = 'L';
+    
+    private readonly Grid2D<char> _grid;
+    private readonly Dictionary<int, Vec2D> _topSlotMap;
+    private readonly Dictionary<Vec2D, int> _botSlotMap;
+
+    public IReadOnlyCollection<int> SlotIndices => _topSlotMap.Keys;
+    
+    private Machine(Grid2D<char> grid)
+    {
+        var slotIndices = Enumerable
+            .Range(start: 0, count: grid.Width)
+            .Where(i => i % 2 == 0)
+            .ToArray();
+        
+        _grid = grid;
+        _topSlotMap = slotIndices.ToDictionary(
+            keySelector:     i => i / 2, 
+            elementSelector: i => new Vec2D(X: i, Y: grid.Height));
+        _botSlotMap = slotIndices.ToDictionary(
+            keySelector:     i => new Vec2D(X: i, Y: 0),
+            elementSelector: i => i / 2);
+    }
+    
+    public static Machine Parse(string[] lines)
+    {
+        return new Machine(grid: Grid2D<char>.MapChars(lines));
+    }
+    
+    public int Drop(int tossSlotIndex, string tokenBehaviour)
+    {
+        int result;
+        var pos = _topSlotMap[tossSlotIndex];
+        var bounce = 0;
+
+        while (!_botSlotMap.TryGetValue(pos, out result))
+        {
+            var below = pos + Vec2D.Down;
+            switch (_grid[below])
+            {
+                case Void:
+                    pos = below;
+                    continue;
+                case Nail:
+                    var behaviour = tokenBehaviour[bounce++ % tokenBehaviour.Length];
+                    var bounceDir = behaviour == Left ? Vec2D.Left : Vec2D.Right;
+                    var targetPos = below + bounceDir;
+                    if (!_grid.Contains(targetPos) || _grid[targetPos] is Nail)
+                    {
+                        targetPos = below - bounceDir;
+                    }
+                    pos = targetPos;
+                    continue;
+                default:
+                    throw new NoSolutionException($"Invalid machine cell '{_grid[below]}' at {below}.");
+            }
+        }
+
+        return Math.Max(0, 2 * (result + 1) - (tossSlotIndex + 1));
+    }
+}
---
+++ Solutions/S02/Q01/Solution.cs
@@ -0,0 +1,82 @@
+using Utilities.Extensions;
+
+namespace Solutions.S02.Q01;
+
+[QuestInfo("Nail Down Your Luck", Topics.Simulation|Topics.Graphs, Difficulty.Hard)]
+public sealed class Solution : SolutionBase
+{
+    public override object Solve(int part)
+    {
+        var input = GetInputLines(part);
+        var chunks = input.ChunkByNonEmpty();
+        var machine = Machine.Parse(chunks[0]);
+        var tokens = chunks[1];
+        
+        return part switch
+        {
+            1 => DropTokens(machine, tokens),
+            2 => MaximizeWinnings(machine, tokens),
+            3 => FindExtrema(machine, tokens),
+            _ => NotSolvedString
+        };
+    }
+    
+    private static int DropTokens(Machine machine, string[] tokens)
+    {
+        return tokens
+            .Select((behaviour, index) => machine.Drop(index, behaviour))
+            .Sum();
+    }
+
+    private static int MaximizeWinnings(Machine machine, string[] tokens)
+    {
+        return tokens
+            .Select(behaviour => machine.SlotIndices.Max(index => machine.Drop(index, behaviour)))
+            .Sum();
+    }
+
+    private static string FindExtrema(Machine machine, string[] tokens)
+    {
+        var slotIndices = machine.SlotIndices.ToArray();
+        var memo = new Dictionary<(int, int), (int, int)>();
+        var (min, max) = SolveDp(tokenIndex: 0, usedSlotsMask: 0);
+        
+        return $"{min} {max}";
+
+        (int min, int max) SolveDp(int tokenIndex, int usedSlotsMask)
+        {
+            if (tokenIndex >= tokens.Length)
+            {
+                return (min: 0, max: 0);
+            }
+            
+            var key = (tokenIndex, usedSlotsMask);
+            if (memo.TryGetValue(key, out var cachedExtrema))
+            {
+                return cachedExtrema;
+            }
+            
+            var minCoins = int.MaxValue;
+            var maxCoins = int.MinValue;
+            
+            for (var i = 0; i < slotIndices.Length; i++)
+            {
+                if ((usedSlotsMask & (1 << i)) != 0)
+                {
+                    continue;
+                }
+                
+                var coins = machine.Drop(slotIndices[i], tokens[tokenIndex]);
+                var (nextMin, nextMax) = SolveDp(
+                    tokenIndex: tokenIndex + 1,
+                    usedSlotsMask: usedSlotsMask | (1 << i));
+                
+                minCoins = Math.Min(minCoins, coins + nextMin);
+                maxCoins = Math.Max(maxCoins, coins + nextMax);
+            }
+            
+            memo[key] = (minCoins, maxCoins);
+            return (minCoins, maxCoins);
+        }
+    }
+}
